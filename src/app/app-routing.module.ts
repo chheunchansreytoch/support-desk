cbbca73402f69cab67de52c2b9a74f9a61da2921
chheunchansreytoch <chheunchansreytoch@gmail.com>
2021-05-25 14:32:30 +0700

    update login form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,21 +3,26 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainFrontLayoutComponent } from './layouts/main-front-layout/main-front-layout.component';
 import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 
 const routes: Routes = [
- {
-   path: '',
-   component: MainFrontLayoutComponent,
-   children: [
-    { path: '', component: HomePageComponent },
-    { path: 'contact-us', component: ContactPageComponent }
-   ]
- },
- {
-   path: '**',
-   component: NotFoundPageComponent
- }
+  {
+    path: '',
+    component: LoginPageComponent
+  },
+  {
+    path: '',
+    component: MainFrontLayoutComponent,
+    children: [
+      { path: 'home', component: HomePageComponent },
+      { path: 'contact-us', component: ContactPageComponent }
+    ]
+  },
+  {
+    path: '**',
+    component: NotFoundPageComponent
+  }
 ];
 
 @NgModule({
